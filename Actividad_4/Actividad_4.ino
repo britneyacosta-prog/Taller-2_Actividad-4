ca15388b564982fabef0305e8764218873affd99
Ninoniix <jsebastian.zamorao@gmail.com.com>
2025-09-05 14:32:06 -0500

    Cambio en el tiempo Sebastian

--- Actividad_4/Actividad_4.ino
+++ Actividad_4/Actividad_4.ino
@@ -9,8 +9,8 @@ void loop() {
   byte receivedData = 0;  // Variable para almacenar el byte recibido
   byte bitCount = 0;    // Contador de bits
   while (digitalRead(2) == HIGH); // Espera hasta que el pin sea LOW
-  delay(50);
-
+  delay(5000);
+//Cambio de Sebastian
   while (bitCount < 8) {
     // Espera a que el bit cambie (transición de LOW a HIGH o HIGH a LOW)
     // Breve espera para estabilización
