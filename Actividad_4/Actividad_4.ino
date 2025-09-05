07752132857dc88ea239d5945e3cd4aa9975f59e
Alejapython <maria.sardi@estudiantesunibague.edu.co>
2025-09-05 16:04:19 -0500

    puse texto extra a imprimir y cambie el pin de entrada

--- Actividad_4/Actividad_4.ino
+++ Actividad_4/Actividad_4.ino
@@ -2,19 +2,19 @@
 // Código para el receptor RX
 void setup() {
   Serial.begin(9600);  // Inicializa la comunicación serial
-  pinMode(2, INPUT);  // Configura el pin de bits como entrada
+  pinMode(3, INPUT);  // Configura el pin de bits como entrada
 }
 
 void loop() {
   byte receivedData = 0;  // Variable para almacenar el byte recibido
   byte bitCount = 0;    // Contador de bits
-  while (digitalRead(2) == HIGH); // Espera hasta que el pin sea LOW
+  while (digitalRead(3) == HIGH); // Espera hasta que el pin sea LOW
   delay(30);
 //Cambio de Sebastian desde rama
   while (bitCount < 8) {
     // Espera a que el bit cambie (transición de LOW a HIGH o HIGH a LOW)
     // Breve espera para estabilización
-    int bit = digitalRead(2); // Lee el bit
+    int bit = digitalRead(3); // cambie el numero del pin de emtrada
     receivedData = (receivedData << 1) | bit;
     bitCount++;
     delay(10);
@@ -28,7 +28,7 @@ void loop() {
 
   
   printBinary(receivedData);    // Imprime el byte en formato binario
-  Serial.print(" (ASCII: ");
+  Serial.print(" El ASCII es: ");//cambie el textooooo
   Serial.print((char)receivedData); // Imprime el carácter ASCII
   Serial.println(")");
   receivedData = 0;
@@ -39,6 +39,7 @@ void loop() {
 // Función para imprimir un byte en formato binario
 void printBinary(byte data) {
   for (int i = 7; i >= 0; i--) {
+    Serial.print("El número en binario es :");//puse mas texto
     Serial.print((data >> i) & 1);  // Imprime cada bit
   }
 }
